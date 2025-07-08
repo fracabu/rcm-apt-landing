8270c08107528cce14c1b52ec9af3731e0fe6b9e
fracabu <100199965+fracabu@users.noreply.github.com>
2025-07-08 23:34:13 +0200

    corretto numero cell

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -260,7 +260,7 @@
                 <div class="flex items-center">
                   <MessageCircleIcon class="text-roma-600 mr-3 h-5 w-5" />
                   <a href="https://wa.me/393123456789" class="text-roma-600 hover:underline">
-                    WhatsApp: +39 312 345 6789
+                    WhatsApp: +39 320 49 33 807
                   </a>
                 </div>
                 <div class="flex items-center">
